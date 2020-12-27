a7f16d3a723dbf72f81f7a401174110cb5828248
HadesHelios <madhadesgames@gmail.com>
2020-12-27 09:25:04 +0100

    Adding rotation to left and right

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -27,11 +27,11 @@ public class Rocket : MonoBehaviour
         }
         if (Input.GetKey(KeyCode.Q))
         {
-            print("Roate Left !");
+            transform.Rotate(Vector3.forward);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            print("Roate Right !");
+            transform.Rotate(-Vector3.forward);
         }
     }
 }
